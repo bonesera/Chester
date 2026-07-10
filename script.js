54580b62d208bba72cd72f940d4ed9ede7120fb8
bonesera <jacobbzzz7c8i5@gmail.com>
2026-07-10 11:39:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function setName() {
+  // BLANK 1: Put the ID of the paragraph element inside the quotes ("username-display")
+  // BLANK 2: Put the text string you want to change it to (Example: "Codey")
+  
+  document.getElementById("username-display").innerHTML = "Chester, Mandy and Berry are all co-workers (Chester is the jester-like-employee who constantly plays practical jokes on Mandy. Meanwhile, Mandy owns the candy shop because she is obsessed with candy.)";
+}
